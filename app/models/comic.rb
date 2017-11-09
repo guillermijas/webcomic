58a02b8229859878f905247f3d136643891ef1aa
Guillermo Mora <guillermijas@gmail.com>
2017-11-09 12:57:58 +0100

    Read comic permissions

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -5,4 +5,8 @@ class Comic < ApplicationRecord
 
   has_attached_file :cover, styles: { medium:'300x300>', thumb: '100x100>' }
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
+
+  def paid_by? (user)
+    Payment.where(user_id: user.id, comic_id: id).first.present?
+  end
 end
